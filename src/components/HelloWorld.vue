80d1f20bcd7465df28ea06b1300360d5531119bb
CyanSalt <c.salt@hotmail.com>
2023-02-14 16:48:01 +0800

    feat: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { BaseProps } from './types'
+
+interface IProps extends BaseProps {
+  bar: string,
+}
+
+const props = defineProps<IProps>()
+</script>
+
+<template>
+  <div>Props: {{ JSON.stringify(props, null, 2) }}</div>
+</template>
