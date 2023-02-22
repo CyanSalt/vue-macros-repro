bcd30fb7a324d7d4d95f02d3482c1cfb22bd69c9
Sun Xiaoran <c.salt@hotmail.com>
2023-02-22 08:20:15 +0000

    Update 2 files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import { getCurrentInstance } from "vue";
+
 const { values = [] } = defineProps<{
-  values?: number[]
-}>()
+  values?: number[];
+}>();
+
+const instance = getCurrentInstance();
 
 function add() {
-  values.push(values.length)
+  values.push(values.length);
+  instance!.proxy!.$forceUpdate();
 }
 </script>
 
 <template>
   <div>
-    <code><pre>{{ JSON.stringify(values) }}</pre></code>
+    <code>
+      <pre>{{ JSON.stringify(values) }}</pre>
+    </code>
     <button @click="add">Add</button>
   </div>
 </template>
